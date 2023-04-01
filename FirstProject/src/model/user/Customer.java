dedd4343edd24495a02daa77296e5cfac597c7b8
yones jamshidi <jamshidiyounes92@gmail.com>
2023-04-01 17:45:27 +0430

    23t commit : add variables and constructor to Customer class

--- FirstProject/src/model/user/Customer.java
+++ FirstProject/src/model/user/Customer.java
@@ -1,4 +1,18 @@
 package model.user;
 
-public class Customer {
+import model.commodity.Commodity;
+import model.connectors.Invoice;
+
+import java.util.ArrayList;
+
+public class Customer extends User {
+    private double credit;
+    private ArrayList<Commodity> cart;
+    private ArrayList<Invoice> shoppinHistory;
+
+    public Customer(String username, String emailAddress, String phoneNumber, String password) {
+        super(username, emailAddress, phoneNumber, password);
+        cart = new ArrayList<>();
+        shoppinHistory = new ArrayList<>();
+    }
 }
